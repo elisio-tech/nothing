144aa51b8ba995bba9545593166e226276fa2bcd
Elisio Augusto <elisiouiux@gmail.com>
2026-05-04 23:04:58 +0100

    tests fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import "@/app/globals.css";
+import Hero from "@/app/components/Hero";
+import Works from "./components/Works";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Hero />
+      <Works />
     </div>
   );
 }
